650a3b123440d1b64f3c84bde79b18d0943ec1aa
jgarcia60 <68722324+jgarcia60@users.noreply.github.com>
2020-12-01 11:08:02 -0500

    added functions in orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,27 @@
 const connection = require('../config/connection.js');
+
+const orm = {
+    selectAll: function(whatToSelect, tableInput) {
+        const queryString = "SELECT ?? FROM ??";
+        connection.query(queryString, [whatToSelect, tableInput], function(err, res) {
+            if (err) throw err;
+            console.log(res);
+        });
+    },
+    insertOne: function(whatToInsert) {
+        const queryString = "INSERT INTO burgers(burger_name, devoured) VALUES(??, false)";
+        connection.query(queryString, [whatToInsert], function(err, res) {
+            if (err) throw err;
+            console.log(res);
+        });
+    },
+    updateOne: function(updatedValue, whatToUpdate) {
+        const queryString = "UPDATE burgers SET devoured = ?? WHERE burger_name = ??";
+        connection.query(queryString, [updatedValue, whatToUpdate], function(err, res) {
+            if (err) throw err;
+            console.log(res);
+        });
+    }
+}
+
+module.exports = orm;
